3355466d049e39f11e3f1cdcefa7daee1043d444
ppgmdev <ptono98@gmail.com>
2024-12-21 12:57:47 -0600

    add lazy and plugins

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 print("hello ppgm")
 
+require("config.lazy")
+
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
