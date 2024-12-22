2bde7975f9fee78dc174c428dbe4a57844c150df
ppgmdev <ptono98@gmail.com>
2024-12-22 11:24:20 -0600

    ftplugins

--- init.lua
+++ init.lua
@@ -4,10 +4,13 @@ require("config.lazy")
 require("set")
 require("remap")
 
+vim.opt.shiftwidth = 4
+
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
 
+-- Highlight when yanking
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking text",
 	group = vim.api.nvim_create_augroup("highligh-yank", {clear = true}),
